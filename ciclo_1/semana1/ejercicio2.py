1b1a4be1a7a3a120c836f3d9111fbab9c5bed635
IsraelArbona <i.arbonammisena.edu.co>
2022-05-01 21:42:34 -0500

    Inicio Proyecto

---
+++ ciclo_1/semana1/ejercicio2.py
@@ -0,0 +1,54 @@
+# declarar variable float
+var1 = 1.35
+#print(var1)
+
+#castear variable de float a entero
+var1 = int(var1)
+#print(var1)
+
+var2 = "100"
+var2 = int(var2) + 100
+#print(var2)
+
+'''
+    -- Errores al declarar variable
+
+    var 1 = 80
+    @var1 = 10
+    1var = 40
+    var-4 = 120
+
+_1 = 10
+print(_1)
+'''
+
+'''
+num1 = num2 = num3 = 200
+print(num1)
+print(num2)
+print(num3)
+
+print(num1,"\n",num2,num3)
+
+num_1, num_2, num_3 = 34, 50, 5.67
+print(num_1)
+print(num_2)
+print(num_3)
+'''
+
+'''
+x = 2 * (5 - 1)
+print(x)
+
+x = (1 + 1) ** (5 - 2)
+print(x)
+
+x = 2 ** 1 + 1
+print(x)
+
+y = 3 * 1 ** 3
+print(y)
+
+x = 2 * 3 / 2
+print(x)
+'''
