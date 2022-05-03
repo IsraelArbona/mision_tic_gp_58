6a0a7c8234d68988a13b76867bdeb8dea5d0a9fb
IsraelArbona <i.arbonammisena.edu.co>
2022-05-03 16:01:06 -0500

    Clase 03 de mayo del 2022

--- ciclo_1/semana1/ejercicio3.py
+++ ciclo_1/semana1/ejercicio3.py
@@ -44,16 +44,129 @@ def operacionSumaP():
 operacionSumaP()
 '''
 
+'''
 def sumarDosnumeros():
     num1 = float(input("Ingrese el numero 1 : "))
     num2 = float(input("Ingrese el numero 2 : "))
     print("La suma de", int(num1),"+",int(num2), "es", int(num1 + num2))
 
-sumarDosnumeros()
+#sumarDosnumeros()
 
 def sumarDosnumerosInt():
     num1 = int(input("Ingrese el numero 1 : "))
     num2 = int(input("Ingrese el numero 2 : "))
     print("La suma de", num1,"+",num2, "es", num1 + num2)
 
-sumarDosnumerosInt()
+#sumarDosnumerosInt()
+
+def raizCuadrada():
+    valor = float(input("Por favor, introduzca un numero a calcular su raiz cuadrada "))
+    raiz = valor ** 0.5
+    return print("La raiz cuadrada de : ", valor, "es", raiz)
+
+#raizCuadrada()
+'''
+
+
+
+'''
+b = 5
+def suma(a,b):
+    return a + b
+
+B = 30
+A = 10
+a = 15
+
+print(suma(a,b))
+'''
+
+def otra_suma(numero1,numero2):
+    print(numero1 + numero2)
+    print("\n")
+    return numero1 + numero2
+
+#resultado = otra_suma(5, 6)
+# print(resultado)
+
+
+#creación de la función
+def mi_funcion(nombre,apellido):
+    miNombre = nombre + apellido
+    return miNombre
+
+#Llamar la función
+#print(mi_funcion("Luis", "Morelo"))
+
+def saludar(nombre, mensaje='Hola'):
+    print(mensaje,nombre)
+
+#saludar('Pepe Grillo')
+
+def mensaje():
+    print("Ingrese por favor el valor")
+
+def sumarDosnumeros():
+    mensaje()
+    num1 = float(input())
+    mensaje()
+    num2 = float(input())
+
+    return print("la suma de",num1,"+",num2,"es igual a: ",num1 + num2)
+
+#llamar la función
+#sumarDosnumeros()
+
+'''
+def primeraFuncion(): # función externa
+    print("\n \"Hola desde la función externa\" \n")
+    def segundaFuncion(): # función interna
+        print("\n \"Hola desde la función interna\" \n")
+    
+    #llamar la función
+    segundaFuncion()
+
+#llamar la función
+#primeraFuncion()
+'''
+
+
+
+'''
+#crear la función primerNumero
+def primerNumero(x):
+    def segundoNumero(y):
+        return x * y
+    return segundoNumero
+
+resultado = primerNumero(2)
+
+print(type(resultado))
+print(type(resultado(5)))
+print(resultado(5))
+'''
+
+
+
+def primeraFuncion():
+    x = 2
+    def segundaFuncion(a):
+        x = 10
+        print(a + x)
+        return x
+    
+    # llamar la función segundaFuncion
+    x = segundaFuncion(3)
+    print(x)
+
+# llamar la función primeraFuncion
+primeraFuncion()
+
+
+
+
+
+
+
+
+
