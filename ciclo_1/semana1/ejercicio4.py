c5834e766721a7487fd8ff8a5b8279596e95a0bc
IsraelArbona <i.arbonammisena.edu.co>
2022-05-05 17:03:56 -0500

    clase 04 de mayo del 2022

--- ciclo_1/semana1/ejercicio4.py
+++ ciclo_1/semana1/ejercicio4.py
@@ -18,13 +18,13 @@ def calPromedio():
     promedio = round( (nota1 + nota2 + nota3 + nota4)/4,1 )
     return promedio
 
-print(calPromedio())
+#print(calPromedio())
 
 def cPromedio(nota1,nota2,nota3,nota4):
     promedio = (nota1 + nota2 + nota3 + nota4)/4
     return round(promedio,1)
 
-print(cPromedio(nota1, nota2, nota3, nota4))
+#print(cPromedio(nota1, nota2, nota3, nota4))
 
 def promedioNotas():
     nota1 = 4.3
@@ -35,4 +35,4 @@ def promedioNotas():
     promedio = (nota1 + nota2 + nota3 + nota4)/4
     return round(promedio,1)
 
-print(promedioNotas())
+#print(promedioNotas())
