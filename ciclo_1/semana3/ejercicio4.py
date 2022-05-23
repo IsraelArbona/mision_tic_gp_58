977011ca359b19ea9a96b2e5ea41a76884e6e0a7
IsraelArbona <i.arbonammisena.edu.co>
2022-05-23 15:47:58 -0500

    Clase 23 de mayo de 2022

--- ciclo_1/semana3/ejercicio4.py
+++ ciclo_1/semana3/ejercicio4.py
@@ -1,5 +1,6 @@
 # Lista paralelas
 
+'''
 nombres1 = ['Juan','Maria','Josue','Marcos','Luz']
 edades2 =  [15    ,32     ,25     ,17      ,18]
 
@@ -17,16 +18,66 @@ print('Nombre de las personas que son mayores o iguales a 18 años')
 for xedad in range(5):
     if edades[xedad] >= 18:
         print('Nombre -', nombres[xedad], ' edad : ', edades[xedad])
-
-
-
-
-
-
-
+'''
 
 
 '''
     print(type(nombres)) # tipo de dato
     print(dir(nombres))  # método del objeto
 '''
+
+
+# listas compuestas.
+
+lista = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+'''
+print(lista)
+print(lista[0])
+print(lista[0][0])
+'''
+
+'''
+for x in range(len(lista[0])):
+    print(lista[0][x])
+'''
+
+'''
+for x in range(len(lista)):
+    for k in range(len(lista[x])):
+        print(lista[x][k])
+'''
+
+# Ejercicio 2
+
+'''
+lista = [[1,3,5,7,9],[2,4,6,8,10]]
+
+for x in range(len(lista)):
+    resultado = 0
+    for k in range(len(lista[x])):
+        resultado = resultado + lista[x][k]
+    
+    print(resultado)
+'''
+
+# Ejercicio 3
+
+lista = [[1],[1,2],[1,2,3],[1,2,3,4],[1,2,3,4,5]]
+
+resultado = 0
+for x in range(len(lista)):
+    for k in range(len(lista[x])):
+        resultado = resultado + lista[x][k]
+
+print(resultado)
+
+
+
+
+
+
+
+
+
+
