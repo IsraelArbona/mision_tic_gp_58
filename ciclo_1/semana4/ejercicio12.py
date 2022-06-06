372af67e01a69145de1df137476837fe78795d2f
IsraelArbona <i.arbonammisena.edu.co>
2022-06-06 15:48:36 -0500

    Clase del 06 junio del 2022

---
+++ ciclo_1/semana4/ejercicio12.py
@@ -0,0 +1,19 @@
+# libreria numpy
+
+import numpy as np
+
+a = np.array(
+    [
+        [1,2,3],
+        [5,6,7],
+        [9,10,11],
+    ]
+)
+
+print(a, '\n')
+
+b = a[:2, 1:3]
+print(b,'\n')
+
+c = a[1:3,:2]
+print(c)
