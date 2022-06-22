9a0d207d58b321feb804580c3c0810ca8bb9863c
IsraelArbona <i.arbonammisena.edu.co>
2022-06-22 16:45:16 -0500

    Clase 22 de junio del 2022

---
+++ ciclo_2/semana1/Ejercicio_1/src/App.java
@@ -0,0 +1,54 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Comentamos una línea de código
+
+        /*
+         * Comentamos un bloque o varias líneas de código,
+         * todo lo comentado es ignorado en la ejecución
+         */
+
+        /**
+         * Este es un comentario de documentación
+         * se usa para el javadoc de nuestro proyecto
+         */
+
+        // Declaración de variables
+
+        /*
+        int x = 0;
+        String nombre = "Juan";
+        double a = 1.4;
+        boolean bNuevo = false;
+        int[] datos;
+
+        var j = 0;
+        var ejemplo = "ejemplo";
+        byte valor_byte = 120;
+        int valor_int = 2384374;
+        var resultado = 0.0;
+
+        resultado = valor_int / a;
+
+        System.out.println(resultado);
+        */
+
+
+        // Caracteres especiales
+
+        System.out.println("Hola, \tGrupo_58");
+        System.out.println("Hola, \bGrupo_58");
+        System.out.println("Hola, \nGrupo_58");
+
+        System.out.println("----------------");
+        System.out.println("Hola, \rGrupo_58");
+        System.out.println("Hola, \fGrupo_58");
+
+        System.out.println("\'Hola, Grupo_58\'");
+        System.out.println("\"Hola, Grupo_58\"");
+
+        var $mivariable = 0;
+        var _mivariable = "0";
+
+    }
+}
