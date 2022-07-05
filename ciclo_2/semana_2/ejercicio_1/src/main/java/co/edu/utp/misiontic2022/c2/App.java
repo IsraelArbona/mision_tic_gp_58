590f701259b04a01af2b3d0e766994b12696395e
IsraelArbona <i.arbonammisena.edu.co>
2022-07-05 15:47:55 -0500

    Clase del 05 Julio del 2022

---
+++ ciclo_2/semana_2/ejercicio_1/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,30 @@
+package co.edu.utp.misiontic2022.c2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /*
+            MiPrimerClase mpc = new MiPrimerClase();
+            MiPrimerClase mpc2 = new MiPrimerClase(20);
+            MiPrimerClase mpc3 = new MiPrimerClase(10,25);
+
+            mpc.incrementarContador(10);
+            System.out.println("Valor del atributo contador mpc es: " + mpc.getContador());
+            mpc.setContador(50);
+            System.out.println("Valor del atributo contador mpc es: " + mpc.getContador());
+
+            System.out.println("Valor del atributo contador mpc2 es: " + mpc2.getContador());
+            mpc2.incrementarContador(80);
+            System.out.println("Valor del atributo contador mpc2 es: " + mpc2.getContador());
+
+            System.out.println("Valor contador mpc3 es: " + mpc3.getContador());
+            System.out.println("Valor numero Horas mpc3 es: " + mpc3.getNumHoras());
+        */
+
+    }
+}
